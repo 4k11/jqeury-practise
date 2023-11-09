6d24fa7f93eea749e08feea14814b723d825dd2e
Akil Dhanesh <akil.dhnx@gmail.com>
2023-11-09 14:01:51 +0530

    c4 logic

--- script.js
+++ script.js
@@ -54,8 +54,77 @@ function colorMatch(one, two, three, four) {
 
 function horizontal() {
   for (var row = 0; row < 6; row++) {
-    for (var col = 0; col < 7; col++){
-        if (colorMatch(r))
+    for (var col = 0; col < 7; col++) {
+      if (
+        colorMatch(
+          reportColor(row, col),
+          reportColor(row, col + 1),
+          reportColor(row, col + 2),
+          reportColor(row, col + 3)
+        )
+      ) {
+        console.log("h passed");
+        win(row, col);
+        return true;
+      } else {
+        console.log("h failed");
+        continue;
+      }
+    }
+  }
+}
+
+function vertical() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 6; row++) {
+      if (
+        colorMatch(
+          reportColor(row, col),
+          reportColor(row + 1, col),
+          reportColor(row + 2, col),
+          reportColor(row + 3, col)
+        )
+      ) {
+        console.log("v passed");
+        win(row, col);
+        return true;
+      } else {
+        console.log("v failed");
+        continue;
+      }
+    }
+  }
+}
+
+function diagonal() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 6; row++) {
+      if (
+        colorMatch(
+          reportColor(row, col),
+          reportColor(row + 1, col + 1),
+          reportColor(row + 2, col + 2),
+          reportColor(row + 3, col + 3)
+        )
+      ) {
+        console.log("d passed");
+        win(row, col);
+        return true;
+      } else if (
+        colorMatch(
+          reportColor(row, col),
+          reportColor(row - 1, col + 1),
+          reportColor(row - 2, col + 2),
+          reportColor(row - 3, col + 3)
+        )
+      ) {
+        console.log("d passed");
+        win(row, col);
+        return true;
+      } else {
+        console.log("d failed");
+        continue;
+      }
     }
   }
 }
